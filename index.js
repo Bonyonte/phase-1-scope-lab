ef025aef1bc011f617d352ce40d3846e4a51027a
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-10 20:12:11 +0300

    commit tree 15ad17e6053dfbfecc551af34d266a651cb95efb

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'Fix the Scope'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+     bestCustomer = 'not bob';
+}
+console.log(bestCustomer());
+function overwriteBestCustomer(){
+       bestCustomer = 'maybe bob';
+}
+console.log(bestCustomer());
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Fix the Scope';
+}
+console.log(changeLeastFavoriteCustomer());
